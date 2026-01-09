62382795e40af44f5867086cab9f623c080299fb
Mihai Bojescu <bojescu.mihai@gmail.com>
2026-01-10 01:51:05 +0200

    feat: Add a host line in /etc/hosts on the machines

--- scripts/3_presetup_hosts.sh
+++ scripts/3_presetup_hosts.sh
@@ -5,4 +5,5 @@ set -ex
 while read ip host; do
     key=$(echo $host | cut -f 1 -d ".")
     ssh -t -i "../keys/$key" "debian@$ip" "sudo -S hostnamectl set-hostname $host" < /dev/tty
+    ssh -t -i "../keys/$key" "debian@$ip" "echo '127.0.0.1     $host' | sudo tee -a /etc/hosts" < /dev/tty
 done < machines.txt
